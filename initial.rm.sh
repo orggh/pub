22d912f7fcafdbc90267f8e07bed1d4c03362db7
R Mockler <rmockler@gmail.com>
2020-12-08 07:49:25 +0000

    initial

--- initial.rm.sh
+++ initial.rm.sh
@@ -1,1 +1,1 @@
-'mkdir orggh;cd orggh;git clone https://rmockler@github.com/orggh/pub.git;cd pub; echo cd .. >> initial.rm.sh ;git add initial.rm.sh git commit -m initial ; git push '
+cd ~/gits&&mkdir $orggh;cd $orggh;git clone https://$USR@github.com/$orggh/pub.git;cd pub&& echo "'!!'" >> initial.rm.sh ;git add initial.rm.sh ;git commit -m initial && git push
